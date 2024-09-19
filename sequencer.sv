4e28bf5fee557401e60e080aa53d186079663976
sravanavaidehi <sravanavaidehi@gmail.com>
2024-09-19 15:09:42 -0500

    sequencer.sv

---
+++ sequencer.sv
@@ -0,0 +1,8 @@
+class f_sqr extends uvm_sequencer #(item);
+  `uvm_component_utils(f_sqr)
+  function new(string name = "f_sqr", uvm_component parent = null);
+    super.new(name,parent);
+  endfunction
+endclass
+
+            
